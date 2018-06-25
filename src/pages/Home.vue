59db94f5c9fc3e312f44a347a7a65ef6f9c2fb6a
hubo36 <hubo11@jd.com>
2018-06-25 13:04:03 +0800

    重构接口字段

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
     <div>
-      <input type="text" ref="symbol" value="iostusdt">
+      <p>{{status}}</p>
+      <input type="text" ref="symbol" value="iost">
+      对
+      <input type="text" ref="symbol2" value="usdt">
       <button @click="subscribe">
           查挂单
       </button>
       <div class="flex-row">
         <div class="flex-1">
+          买单({{bidsList.length}})
           <ul>
             <li
               v-for="(item, index) in bidsList"
@@ -19,6 +23,7 @@
           </ul>
         </div>
         <div class="flex-1">
+          卖单({{asksList.length}})
           <ul>
             <li
               v-for="(item, index) in asksList"
@@ -43,20 +48,30 @@ export default {
   name: "Home",
   data() {
     return {
-      bidsList: [],
-      asksList: [],
+      status: 'ws未连接',
+      bidsList: [], // 买单
+      asksList: [], // 卖单
     };
   },
   mounted() {
     this.ws = new WebSocket("ws://localhost:3000/huobi");
+    this.status = 'ws未连接';
     this.ws.onopen = () => {
       // Web Socket 已连接上，使用 send() 方法发送数据
       this.ws.send(JSON.stringify({
-        "key": `huobi`,
+        "type": `ws-huobi`,
         "value": 'open',
       }));
+      this.status = 'ws-server已连接';
     };
-
+    this.ws.onclose = () => {
+      // 关闭 websocket
+      this.status = 'ws-server连接已关闭...';
+    };
+    this.ws.onerror = (err) => {
+      // 关闭 websocket
+      this.status = err;
+    };
     this.ws.onmessage = (ev) => {
       console.log(ev.data)
       var data = JSON.parse(ev.data);
@@ -64,30 +79,30 @@ export default {
         this.bidsList = getSameAmount(data.tick.bids);
         this.asksList = getSameAmount(data.tick.asks);
       }
-      
+
+      switch(data.type) {
+        case 'WS_HUOBI':
+        this.status = 'WS_HUOBI:' + data.value;
+      }
+
     };
 
-    this.ws.onclose = function() {
-      // 关闭 websocket
-      console.log("连接已关闭...");
-    };
-    this.ws.onerror = function(err) {
-      // 关闭 websocket
-      console.log(err);
-    };
+    
   },
   beforeDestroy() {
     this.ws.send(JSON.stringify({
-      "key": `huobi`,
-      "value": 'close',
+      type: `ws-huobi`,
+      value: 'close',
     }));
+    this.ws.close();
   },
   methods: {
     subscribe() {
-      let value = this.$refs.symbol.value;
+      let value = this.$refs.symbol.value + this.$refs.symbol2.value;
       this.ws.send(JSON.stringify({
-        "key": `huobi`,
-        "symbols": `${value}`
+        type: `ws-huobi`,
+        value: 'subscribe',
+        symbols: `${value}`
       }));
     }
   }
