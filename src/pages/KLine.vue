f8077e66901ae22beb39b3c85491e71252bdb612
hubo36 <hubo11@jd.com>
2018-09-30 16:44:20 +0800

    IntervalTask

--- src/pages/KLine.vue
+++ src/pages/KLine.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <div class="flex-row">
-
+      <p>{{data}}</p>
+      <button @click="act">激活</button>
+      <button @click="doIt">do</button>
     </div>
   </div>
 </template>
@@ -9,6 +11,8 @@
 <script>
 import config from '@/config';
 import { getKLine } from '@/api/huobiREST';
+
+import IntervalTask from '@/utils/IntervalTask';
 export default {
   name: "Kline",
   components: {
@@ -16,7 +20,8 @@ export default {
   },
   data() {
     return {
-      list: []
+      list: [],
+      data: null,
     };
   },
   created() {
@@ -26,11 +31,23 @@ export default {
     //     console.log(res)
     // })
 
+    this.timer = new IntervalTask({disTime: 2000});
+    
   },
   beforeDestroy() {
 
   },
   methods: {
+    act() {
+      this.timer.activate();
+    },
+    doIt() {
+      setInterval(() => {
+        this.timer.do(() => {
+          this.data = Math.random();
+        });
+      }, 1000);
+    }
   }
 };
 </script>
