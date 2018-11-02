c35586182b80f566aedb67bbc3cbd46840315017
hubo36 <hubo36@jd.com>
2018-11-03 00:35:22 +0800

    .

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ let originhost = 'http://182.61.43.233:3000';
 /* 是否移动端 */
 var isMobile = !!/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent);
 const appConfig = {
-  API_HOST: localhost,
+  API_HOST: originhost,
   wsHost: '182.61.43.233:3000',
   API_HUOBI_HOST: 'https://api.huobi.br.com',
   isMobile,
